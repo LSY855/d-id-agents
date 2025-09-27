14bf7ce671ccd011fe8eaf2880318e37c9b2a338
LSY855 <seoyounglee030416@gmail.com>
2025-09-27 16:57:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ AGENT_ID = st.secrets["DID_AGENT_ID"]
 
 st.title("D-ID AI Agent")
 
-html_code = """
+html_code = f"""
 <div id="did-agent-container" style="width: 80%; height: 600px;"></div>
 <script type="module"
-      src="https://agent.d-id.com/v2/index.js"
-      data-mode="fabio"
-      data-client-key="{CLIENT_KEY}"
-      data-agent-id="{AGENT_ID}"
-      data-name="did-agent"
-      data-monitor="true"
-       data-target-id="did-agent-container">
+    src="https://agent.d-id.com/v2/index.js"
+    data-mode="full"
+    data-client-key="{CLIENT_KEY}"
+    data-agent-id="{AGENT_ID}"
+    data-name="did-agent"
+    data-monitor="true"
+    data-target-id="did-agent-container">
 </script>
 """
 
